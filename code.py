03bcacf8fb22ee985f40315e4539efc0b20e7d0a
Anuta-P <anniparfenova@edu.hse.ru>
2025-09-21 21:55:48 +0300

    Update code.py
    Заменила сложение на вычитание через web-интерфейс

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 print("Hello, world!")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number"))
-result = num1 + num2
+result = num1 - num2
 print("The result is:", result)
